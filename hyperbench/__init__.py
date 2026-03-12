58c3e78d1b5a97e053d7e278eab4015a8b843f47
Tiziano <tizianocitro98@gmail.com>
2026-03-12 13:02:08 +0100

    fix: ignore torch warnings

---
+++ hyperbench/__init__.py
@@ -0,0 +1,4 @@
+import warnings
+
+warnings.filterwarnings("ignore", message="Sparse CSR tensor support is in beta state")
+warnings.filterwarnings("ignore", message="`torch_geometric.distributed` has been deprecated")
