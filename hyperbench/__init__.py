bed663e05e51e8bde65a01ff8803206f3f48f66a
Tiziano <tizianocitro98@gmail.com>
2026-04-10 11:54:56 +0200

    fix: ignore torch warnings

--- hyperbench/__init__.py
+++ hyperbench/__init__.py
@@ -1,8 +1,9 @@
 import warnings
 
-warnings.filterwarnings("ignore", message="Sparse CSR tensor support is in beta state")
-warnings.filterwarnings("ignore", message="`torch_geometric.distributed` has been deprecated")
+
 warnings.filterwarnings("ignore", message=".*torch.jit.script.*is not supported in Python 3.14.*")
-warnings.filterwarnings(
-    "ignore", message="Sparse invariant checks are implicitly disabled:UserWarning"
-)
+warnings.filterwarnings("ignore", message="`torch_geometric.distributed` has been deprecated")
+warnings.filterwarnings("ignore", message=".*isinstance.*LeafSpec.*is deprecated.*")
+
+warnings.filterwarnings("ignore", message="Sparse CSR tensor support is in beta state")
+warnings.filterwarnings("ignore", message="Sparse invariant checks are implicitly disabled")
