5dc1494e09f5c2a6591cf19f432d8caf3713b9b4
Tiziano <tizianocitro98@gmail.com>
2026-04-22 17:03:49 +0200

    feat: add Node2Vec model

--- hyperbench/models/__init__.py
+++ hyperbench/models/__init__.py
@@ -2,5 +2,6 @@ from .common_neighbors import CommonNeighbors
 from .hgnn import HGNN
 from .hypergcn import HyperGCN
 from .mlp import MLP, SLP
+from .node2vec import Node2Vec
 
-__all__ = ["CommonNeighbors", "HGNN", "HyperGCN", "MLP", "SLP"]
+__all__ = ["CommonNeighbors", "HGNN", "HyperGCN", "MLP", "Node2Vec", "SLP"]
