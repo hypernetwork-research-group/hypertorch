880a6929e68bff912d088c71bf7017c0c7829f95
Tiziano <tizianocitro98@gmail.com>
2026-04-22 17:04:00 +0200

    feat: add Node2Vec HLP module

--- hyperbench/hlp/__init__.py
+++ hyperbench/hlp/__init__.py
@@ -3,6 +3,7 @@ from .hgnn_hlp import HGNNHlpModule, HGNNEncoderConfig
 from .hlp import HlpModule
 from .hypergcn_hlp import HyperGCNHlpModule, HyperGCNEncoderConfig
 from .mlp_hlp import MLPHlpModule, MlpEncoderConfig
+from .node2vec_hlp import Node2VecEncoderConfig, Node2VecHlpModule
 
 __all__ = [
     "CommonNeighborsHlpModule",
@@ -13,4 +14,6 @@ __all__ = [
     "HyperGCNHlpModule",
     "MlpEncoderConfig",
     "MLPHlpModule",
+    "Node2VecEncoderConfig",
+    "Node2VecHlpModule",
 ]
