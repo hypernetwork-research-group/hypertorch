4e48ca3bd5ce7e4407ceed9e6bede01765ead22c
Tiziano <tizianocitro98@gmail.com>
2026-05-13 14:31:07 +0200

    feat: add villain-based node and hyperedgee enrichers

--- hyperbench/nn/__init__.py
+++ hyperbench/nn/__init__.py
@@ -10,6 +10,8 @@ from .enricher import (
     ABHyperedgeWeightsEnricher,
     LaplacianPositionalEncodingEnricher,
     Node2VecEnricher,
+    VilLainHyperedgeAttrEnricher,
+    VilLainNodeFeatureEnricher,
 )
 from .loss import NHPRankingLoss, VilLainLoss, VilLainLossParts
 from .scorer import CommonNeighborsScorer, NeighborScorer
@@ -33,6 +35,8 @@ __all__ = [
     "Node2VecEnricher",
     "NodeAggregator",
     "NodeEnricher",
+    "VilLainHyperedgeAttrEnricher",
     "VilLainLoss",
     "VilLainLossParts",
+    "VilLainNodeFeatureEnricher",
 ]
