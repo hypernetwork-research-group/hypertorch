a1175a52a38c031b6c6ed2871089e91fcf851814
ddevin96 <danieledevinco1996@gmail.com>
2026-05-26 17:11:07 +0200

    fix: add script to count cutoff - real threshold for integration tests

--- hyperbench/integration_tests/data/enricher_integration_test.py
+++ hyperbench/integration_tests/data/enricher_integration_test.py
@@ -15,16 +15,18 @@ from hyperbench.data import LaplacianPositionalEncodingEnricher
 
 NUM_FEATURES = 8
 
-#### Why we limit the number of nodes and hyperedges in the tests to 5000? ####
+#### Why we limit the number of nodes and hyperedges in the tests to 6000? ####
 #
 #
 # Some of the dataset have a very large number of nodes and hyperedges,
 # which can lead to very long runtimes for the enrichers, especially the
 # more complex ones like VilLain. To ensure that our integration tests run in a
-# reasonable amount of time, we limit the number of nodes and hyperedges to 5000
+# reasonable amount of time, we limit the number of nodes and hyperedges to 6000
 # for the enrichment tests. This allows us to test the functionality of the
 # enrichers without running into excessively long test times, while still providing
 #  a meaningful test of their behavior on reasonably sized datasets.
+# With the threshold of 6000 nodes and hyperedges, we cover the 74% of the datasets.
+# The datasets.py in the scripts folder contains a function that calculates the node count cutoff to cover 75% of the datasets.
 
 
 @pytest.mark.parametrize(
@@ -34,8 +36,10 @@ NUM_FEATURES = 8
 @pytest.mark.integration
 def test_lpe_node_enricher(dataset_name):
     dataset = get_dataset_by_name(dataset_name)
-    if dataset.hdata.num_nodes > 5000 or dataset.hdata.num_hyperedges > 5000:
-        pass
+    if dataset.hdata.num_nodes > 6000 or dataset.hdata.num_hyperedges > 6000:
+        pytest.skip(
+            f"Dataset {dataset_name} has more than 6000 nodes or hyperedges, skipping LaplacianPositionalEncoding node enricher test to avoid long runtimes."
+        )
     _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
     to_enrich_dataset.enrich_node_features(
         enricher=LaplacianPositionalEncodingEnricher(
@@ -53,8 +57,10 @@ def test_lpe_node_enricher(dataset_name):
 @pytest.mark.integration
 def test_n2v_node_enricher(dataset_name):
     dataset = get_dataset_by_name(dataset_name)
-    if dataset.hdata.num_nodes > 5000 or dataset.hdata.num_hyperedges > 5000:
-        pass
+    if dataset.hdata.num_nodes > 6000 or dataset.hdata.num_hyperedges > 6000:
+        pytest.skip(
+            f"Dataset {dataset_name} has more than 6000 nodes or hyperedges, skipping Node2Vec node enricher test to avoid long runtimes."
+        )
     _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
     to_enrich_dataset.enrich_node_features(
         enricher=Node2VecEnricher(
@@ -81,8 +87,10 @@ def test_n2v_node_enricher(dataset_name):
 @pytest.mark.integration
 def test_fill_value_hyperedge_enricher(dataset_name):
     dataset = get_dataset_by_name(dataset_name)
-    if dataset.hdata.num_nodes > 5000 or dataset.hdata.num_hyperedges > 5000:
-        pass
+    if dataset.hdata.num_nodes > 6000 or dataset.hdata.num_hyperedges > 6000:
+        pytest.skip(
+            f"Dataset {dataset_name} has more than 6000 nodes or hyperedges, skipping FillValueHyperedgeAttrsEnricher test to avoid long runtimes."
+        )
     _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
     to_enrich_dataset.enrich_hyperedge_attr(
         enricher=FillValueHyperedgeAttrsEnricher(fill_value=1.0),
@@ -97,8 +105,10 @@ def test_fill_value_hyperedge_enricher(dataset_name):
 @pytest.mark.integration
 def test_villain_hyperedge_enricher(dataset_name):
     dataset = get_dataset_by_name(dataset_name)
-    if dataset.hdata.num_nodes > 5000 or dataset.hdata.num_hyperedges > 5000:
-        pass
+    if dataset.hdata.num_nodes > 6000 or dataset.hdata.num_hyperedges > 6000:
+        pytest.skip(
+            f"Dataset {dataset_name} has more than 6000 nodes or hyperedges, skipping VilLain hyperedge enricher test to avoid long runtimes."
+        )
     _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
     to_enrich_dataset.enrich_hyperedge_attr(
         enricher=VilLainHyperedgeAttrsEnricher(
@@ -123,8 +133,10 @@ def test_villain_hyperedge_enricher(dataset_name):
 @pytest.mark.integration
 def test_villain_node_enricher(dataset_name):
     dataset = get_dataset_by_name(dataset_name)
-    if dataset.hdata.num_nodes > 5000 or dataset.hdata.num_hyperedges > 5000:
-        pass
+    if dataset.hdata.num_nodes > 6000 or dataset.hdata.num_hyperedges > 6000:
+        pytest.skip(
+            f"Dataset {dataset_name} has more than 6000 nodes or hyperedges, skipping VilLain node enricher test to avoid long runtimes."
+        )
     _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
     to_enrich_dataset.enrich_node_features(
         enricher=VilLainEnricher(
@@ -137,20 +149,6 @@ def test_villain_node_enricher(dataset_name):
             num_epochs=3,
             learning_rate=0.01,
             verbose=False,
-        ),
-        enrichment_mode="replace",
-    )
-
-
-if __name__ == "__main__":
-    dataset = get_dataset_by_name("patent")
-    print(f"Original dataset: {dataset}")
-    _, _, to_enrich_dataset = split_dataset(dataset=dataset, node_space_setting="inductive")
-    print(f"Dataset to enrich: {to_enrich_dataset}")
-    to_enrich_dataset.enrich_node_features(
-        enricher=LaplacianPositionalEncodingEnricher(
-            num_features=NUM_FEATURES,
-            num_nodes=to_enrich_dataset.hdata.num_nodes,
         ),
         enrichment_mode="replace",
     )
---
+++ scripts/datasets.py
@@ -0,0 +1,21 @@
+from hyperbench.data import list_datasets, get_dataset_by_name
+
+
+def __find_lowest_limit_to_number_of_nodes_for_50_percent_coverage():
+    dataset_names = list_datasets()
+    node_counts = []
+    map = {}
+    for dataset_name in dataset_names:
+        dataset = get_dataset_by_name(dataset_name)
+        node_counts.append(dataset.hdata.num_nodes)
+        map[dataset_name] = dataset.hdata.num_nodes
+
+    node_counts.sort()
+    cutoff_index = int(0.75 * len(node_counts))
+    cutoff_value = node_counts[cutoff_index]
+    filter = [count for count in node_counts if count <= 6500]
+
+    print(
+        f"Node counts for datasets with 6000 or fewer nodes: {len(filter)} out of {len(node_counts)} - {len(filter) / len(node_counts) * 100:.2f}% of datasets."
+    )
+    print(f"To cover 75% of datasets, we need to set the node limit to {cutoff_value} nodes.")
